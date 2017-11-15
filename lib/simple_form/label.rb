17317f14083cfda9b9652e6ba7776caf1308358a
吉翔 <jx4554@gmail.com>
2017-11-15 11:58:21 +0800

    required fields adds default text mark to labels, allowing i18n for translation

--- lib/simple_form/label.rb
+++ lib/simple_form/label.rb
@@ -5,13 +5,37 @@ module SimpleForm
 
     def generate_label
       return '' if @options[:label] == false
-      html_options = { :class => "#{@input_type} #{required_class}" }
-      label(@attribute, @options[:label] || translate_label, html_options)
+      html_options = { :class => "#{@input_type} #{required_class}".strip }
+      label(@attribute, label_text, html_options)
+    end
+
+    def label_text
+      required_text << (@option[:label] || translate_label)
+    end
+
+    def required_text
+      attribute_required? ? translate_requred_string : ''
     end
 
     def translate_label
       default = @object.try(:human_attribute_name, @attribute.to_s) || @attribute.to_s.humanize
-      options[:label] ||= I18n.t("views.labels.#{@object_name}.#{@attributes}", default: default)
+      I18n.t("#{@object_name}.#{@attributes}", :scope => :"views.labels", :default => default)
+    end
+
+    def translate_required_string
+      translate_string(:required_string, :default => %[<abbr title="#{translate_required_text}">#{translate_required_mark}</abbr>])
+    end
+
+    def translate_required_text
+      translate_string(:required_text, :default => 'required')
+    end
+
+    def translate_required_mark
+      translate_string(:required_mark, :default => '*')
+    end
+
+    def translate_string(key, params={})
+      I18n.(key, {:scope => :simple_form}.merge!(params))
     end
   end
 end
