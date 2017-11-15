fecc03b338e426f2331849dff7ac0afdfd1a412d
吉翔 <jx4554@gmail.com>
2017-11-15 08:57:17 +0800

    refactoring tests and splitting files

--- lib/simple_form/form_builder.rb
+++ lib/simple_form/form_builder.rb
@@ -5,11 +5,23 @@ module SimpleForm
       input_type = (options.delete(:as) || default_input_type(attribute)).to_sym
       html_options = options.delete(:html) || {}
       html_options[:class] = "#{html_options[:class]} #{input_type}".strip
-      case input_type
+
+      input_filed case input_type
         when :boolean then check_box(attribute, html_options)
         when :text    then text_area(attribute, html_options)
         else text_field(attribute, html_options)
       end
+
+      label = if options[:label] == false
+                ''
+              else
+                unless options[:label]
+                  default = @object.try(:human_attribute_name, attribute.to_s) || attribute.to_s.humanize
+                  options[:label] ||= I18n.t("views.labels.#{attribute}", default: default)
+                end
+                label(attribute, options[:label])
+              end
+      label << input_field
     end
 
     private
