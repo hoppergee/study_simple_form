8f0b811d2bcad8a9e58fe0d6186759f0121212e9
吉翔 <jx4554@gmail.com>
2017-11-15 09:03:56 +0800

    Start refactoring label and input

--- lib/simple_form/form_builder.rb
+++ lib/simple_form/form_builder.rb
@@ -2,29 +2,36 @@ module SimpleForm
   class FormBuilder < ActionView::Helpers::FormBuilder
 
     def input(attribute, options={})
-      input_type = (options.delete(:as) || default_input_type(attribute)).to_sym
-      html_options = options.delete(:html) || {}
-      html_options[:class] = "#{html_options[:class]} #{input_type}".strip
+      @attribute, @options = attribute, options
 
-      input_filed case input_type
-        when :boolean then check_box(attribute, html_options)
-        when :text    then text_area(attribute, html_options)
-        else text_field(attribute, html_options)
-      end
+      label = generate_label
+      input = generate_input
 
-      label = if options[:label] == false
-                ''
-              else
-                unless options[:label]
-                  default = @object.try(:human_attribute_name, attribute.to_s) || attribute.to_s.humanize
-                  options[:label] ||= I18n.t("views.labels.#{attribute}", default: default)
-                end
-                label(attribute, options[:label])
-              end
-      label << input_field
+      label << input
     end
 
     private
+
+    def generate_label
+      return '' if @options[:label] == false
+      unless label_text = @options[:label]
+        default = @object.try(:human_attribute_name, attribute.to_s) || attribute.to_s.humanize
+        options[:label] ||= I18n.t("views.labels.#{attribute}", default: default)
+      end
+      label(@attribute, label_text)
+    end
+
+    def generate_input
+      input_type = (@options[:as] || default_input_type).to_sym
+      html_options = @options.delete(:html) || {}
+      html_options[:class] = "#{html_options[:class]} #{input_type}".strip
+
+      input_filed case input_type
+        when :boolean then check_box(@attribute, html_options)
+        when :text    then text_area(@attribute, html_options)
+        else text_field(@attribute, html_options)
+      end
+    end
 
     def default_input_type(attribute)
       input_type = @object.try(:column_for_attribute, attribute)
